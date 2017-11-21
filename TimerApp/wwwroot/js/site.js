10e7285324f94a130af75f5e5b29797e9f3b9246
Tyler Hebert <txh3285@louisiana.edu>
2017-11-21 10:44:43 -0600

    Added seperation coding for each page. Added Datatables.

--- TimerApp/wwwroot/js/site.js
+++ TimerApp/wwwroot/js/site.js
@@ -1,1 +1,35 @@
-﻿// Write your Javascript code.
+﻿// Write relevant code in the section for that page.
+/* GENERAL CODE */
+
+/* LOGIN PAGE */
+if (location.href.includes("login")) {
+
+}
+/* PUNCH CLOCK PAGE */
+else if (location.href.includes("clock")) {
+
+}
+/* TIMESHEET PAGE */
+else if (location.href.includes("timesheet")) {
+
+}
+/* CONTACT INFORMATION PAGE*/
+else if (location.href.includes("coninf")) {
+
+}
+/* CHANGE PASSWORD PAGE */
+else if (location.href.includes("pass")) {
+
+}
+/* SCHEDULE PAGE */
+else if (location.href.toLowerCase().includes("schedule")) {
+    $('#scheduleTable').DataTable();
+}
+/* SUPERVISOR SCHEDULING PAGE */
+else if (location.href.includes("supschedule")) {
+
+}
+/* USER ACCOUNTS PAGE */
+else if (location.href.includes("usac")) {
+
+}
